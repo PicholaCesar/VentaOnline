f393922ee19ff887e10f8db0856e4080136f11aa
PicholaCesar <100111940+PicholaCesar@users.noreply.github.com>
2022-03-13 20:19:46 -0600

    venta online

---
+++ index.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const app = require('./app');
+
+mongoose.Promise = global.Promise;                                                                  //function (){}
+mongoose.connect('mongodb://localhost:27017/VentaOnline', { useNewUrlParser: true, useUnifiedTopology: true })
+.then(()=>{
+    console.log("Conexion a la base de datos");
+
+    app.listen(3000, function () {
+        console.log("servidor Corriendo en el puerto 3000")
+    })
+
+}).catch(error => console.log(error));
+
